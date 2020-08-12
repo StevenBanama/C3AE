1. fix cpu loader in tensorflow 2. add muti-face 3.add mixed-precision

nets/C3AE_expand.py
@@ -25,7 +25,7 @@ from tensorflow.keras import regularizers
 from tensorflow.keras.backend import l2_normalize
 from tensorflow.keras import backend as K
 from tensorflow.keras.constraints import unit_norm
-from utils import focal_loss, ThresCallback, CosineAnnealingScheduler, MishActivation, MishActivation6, config_cpu, config_gpu
+from utils import focal_loss, ThresCallback, CosineAnnealingScheduler, MishActivation, MishActivation6, config_cpu, config_gpu, model_refresh_without_nan
 
 
 
@@ -213,12 +213,15 @@ def preprocessing(dataframes, batch_size=64, category=12, interval=10, is_traini
 
 def train(params):
     from utils import reload_data
+    if params.fp16:
+        os.environ['TF_AUTO_MIXED_PRECISION_GRAPH_REWRITE_IGNORE_PERFORMANCE'] = '1'
+
     sample_rate, seed, batch_size, category, interval = 0.8, 2019, params.batch_size, params.category + 2, int(math.ceil(100. / params.category))
     lr = params.learning_rate
     data_dir, file_ptn = params.dataset, params.source
     dataframes = reload_data(data_dir, file_ptn)
-    print("------", len(dataframes))
     trainset, testset = train_test_split(dataframes, train_size=sample_rate, test_size=1-sample_rate, random_state=seed)
+    print(testset.gender)
     train_gen = preprocessing(trainset, dropout=params.dropout, category=category, interval=interval)
     validation_gen = preprocessing(testset, dropout=0, is_training=False, category=category, interval=interval)
     print(trainset.groupby(["age"])["age"].agg("count"))
@@ -229,12 +232,11 @@ def train(params):
     gender_dist = [trainset["gender"][trainset.gender == 0].count(), trainset["gender"][trainset.gender == 1].count()]
     print(age_dist, gender_dist)
 
-
     models = build_net3(category, using_SE=params.se_net, using_white_norm=params.white_norm)
     if params.pretrain_path:
         #models = load_model(params.pretrain_path, custom_objects={"pool2d": pool2d, "ReLU": ReLU, "BatchNormalization": BatchNormalization, "tf": tf, "focal_loss_fixed": focal_loss(age_dist)})
-        models.load_weights(params.pretrain_path)
-        print("load!!!")
+        ret = models.load_weights(params.pretrain_path)
+        model_refresh_without_nan(models)
 
     #optim = SGD(lr=lr, momentum=0.9)
     if params.freeze:
@@ -248,6 +250,8 @@ def train(params):
 
     epoch_nums = 160
     optim = Adam(lr=lr)
+    if params.fp16:
+        optim = tf.train.experimental.enable_mixed_precision_graph_rewrite(optim)
 
     print("-----outputs-----", models.outputs)
 
@@ -280,7 +284,7 @@ def init_parse():
     parser = argparse.ArgumentParser(
         description='C3AE retry')
     parser.add_argument(
-        '-s', '--save_path', default="./model/c3ae_model_v2_{epoch}_{val_age_mae:02f}-{val_gender_acc:.3f}", type=str,
+        '-s', '--save_path', default="./model/c3ae_model_v2_$message$_{epoch}_{val_age_mae:02f}-{val_gender_acc:.3f}", type=str,
         help='the best model to save')
     parser.add_argument(
         '-l', '--log_dir', default="./logs", type=str,
@@ -347,7 +351,14 @@ def init_parse():
         '-lr', '--learning-rate', default="0.002", type=float,
         help='learning rate')
 
+    parser.add_argument(
+        '-fp16', dest="fp16", action='store_true',
+        help='mix precision training')
+
+
     params = parser.parse_args()
+    params.save_path = params.save_path.replace("$message$", params.message)
+    print("!!-----", params.save_path)
     config_gpu() if params.gpu else config_cpu()
     return params
 

nets/test.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.append("./")
 import cv2
+import numpy as np
 import mxnet as mx
 from detect.mx_mtcnn.mtcnn_detector import MtcnnDetector
 from preproccessing.dataset_proc import gen_face, gen_boundbox
@@ -25,23 +26,23 @@ def load_C3AE(params):
     return models
 
 def load_C3AE2(params):
-    from C3AE_expand import build_net3 
+    from C3AE_expand import build_net3, model_refresh_without_nan 
     models = build_net3(12, using_SE=params.se_net, using_white_norm=params.white_norm)
     if params.model_path:
         models.load_weights(params.model_path)
+        model_refresh_without_nan(models) ## hot fix which occur non-scientice gpu or cpu
     return models
 
 def predict(models, img, save_image=False):
     try:
-        bounds, lmarks = gen_face(MTCNN_DETECT, img)
+        bounds, lmarks = gen_face(MTCNN_DETECT, img, only_one=False)
         ret = MTCNN_DETECT.extract_image_chips(img, lmarks, padding=0.4)
     except Exception as ee:
         ret = None
         print(img.shape, ee)
     if not ret:
         print("no face")
-        return img
-    print(bounds, lmarks)
+        return img, None
     padding = 200
     new_bd_img = cv2.copyMakeBorder(img, padding, padding, padding, padding, cv2.BORDER_CONSTANT)
     bounds, lmarks = bounds, lmarks
@@ -67,17 +68,17 @@ def predict(models, img, save_image=False):
         age, gender = None, None
         if result and len(result) == 3:
             age, _, gender = result
-            age_label, gender_label = age[-1][-1], "female" if gender[-1][0] > gender[-1][1] else "male"
+            age_label, gender_label = age[-1][-1], "F" if gender[-1][0] > gender[-1][1] else "M"
         elif result and len(result) == 2:
             age, _  = result
             age_label, gender_label = age[-1][-1], "unknown"
         else:
            raise Exception("fatal result: %s"%result)
-        cv2.putText(new_bd_img, 'age:%s gender: %s'%(age_label, gender_label), (int(bounds[pidx][0]), int(bounds[pidx][2])), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (25, 2, 175), 2)
+        cv2.putText(new_bd_img, '%s %s'%(int(age_label), gender_label), (padding + int(bounds[pidx][0]), padding + int(bounds[pidx][1])), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (25, 2, 175), 2)
     if save_image:
         print(result)
         cv2.imwrite("igg.jpg", new_bd_img)
-    return new_bd_img
+    return new_bd_img, (age_label, gender_label)
 
 def test_img(params):
     img = cv2.imread(params.image)
@@ -96,11 +97,42 @@ def video(params):
 
         if not ret:
             continue
-        img = predict(models, img) 
+        img, _ = predict(models, img) 
         if img is not None:
             cv2.imshow("result", img)
         if cv2.waitKey(3) == 27:
             break
+
+def load_csv(file_path):
+    import base64
+    result = []
+    with open(file_path, "r") as fd:
+        lines = fd.readlines()
+        for line in lines:
+            num, gender, img_str = line.split(",")
+            arr = base64.b64decode(img_str)
+            content = np.frombuffer(arr, np.uint8)
+            cv_image = cv2.imdecode(content, 1)
+            result.append((gender, cv_image))
+    return result
+
+def load_local_ano(params):
+    result = load_csv("fb4df376a1244c2e2b3f9384ef3bace5.csv")
+    models = load_branch(params)
+    counter, skip = 0, 0
+    for idx, (gender, img) in enumerate(result):
+        img, labels = predict(models, img, True)
+        if labels is None:
+            skip += 1
+            continue
+        age, pgender = labels
+        print("!!!,", pgender)
+        if (gender == "male" and pgender == "F") or (gender == "female" and pgender == "M"):
+            cv2.imwrite("online/%s_%s_%s.jpg"%(idx, gender, pgender), img)
+        else:
+            counter += 1
+        print(gender, pgender)
+    print("%s/ %s /%s"%(counter, skip, len(result)))
 
 def init_parse():
     import argparse
@@ -136,6 +168,7 @@ def init_parse():
 
 if __name__ == "__main__":
     params = init_parse()
+    #load_local_ano(params)
     if params.video:
         video(params)
     else:

nets/utils.py
@@ -116,6 +116,32 @@ class ThresCallback(Callback):
         if K.get_value(self.watch_dog) <= thres:
              K.set_value(self.candi, val)
 
+def model_refresh_without_nan(models):
+    import numpy as np                                                                                                                 
+    valid_weights = []
+    for l in models.get_weights():                                                                                                     
+        if np.isnan(l).any():
+            valid_weights.append(np.nan_to_num(l))
+            print("!!!!!", l)
+        else:
+            valid_weights.append(l) 
+    models.set_weights(valid_weights)
+
+
+def model_refresh_without_nan(models):
+    '''
+        https://github.com/tensorflow/tensorflow/issues/38698
+    '''
+    import numpy as np                                                                                                                 
+    valid_weights = []                                                                                                                 
+    for l in models.get_weights():                                                                                                     
+        if np.isnan(l).any():
+            print("!!!!!", l)
+            valid_weights.append(np.nan_to_num(l))                                                                                     
+        else:                                                                                                                          
+            valid_weights.append(l)
+    models.set_weights(valid_weights)
+
 def focal_loss(classes_num, gamma=2., alpha=.25, e=0.1):
     # classes_num contains sample number of each classes
     # copy from https://github.com/maozezhong/focal_loss_multi_class/blob/master/focal_loss.py
@@ -130,7 +156,7 @@ def focal_loss(classes_num, gamma=2., alpha=.25, e=0.1):
         #1# get focal loss with no balanced weight which presented in paper function (4)
         zeros = array_ops.zeros_like(prediction_tensor, dtype=prediction_tensor.dtype)
         one_minus_p = array_ops.where(tf.greater(target_tensor,zeros), target_tensor - prediction_tensor, zeros)
-        FT = -1 * (one_minus_p ** gamma) * tf.math.log(tf.clip_by_value(prediction_tensor, 1e-8, 1.0))
+        FT = -1 * (one_minus_p ** gamma) * tf.math.log(tf.clip_by_value(prediction_tensor, 1e-6, 1.0))
 
         #2# get balanced weight alpha
         classes_weight = array_ops.zeros_like(prediction_tensor, dtype=prediction_tensor.dtype)
@@ -153,7 +179,6 @@ def focal_loss(classes_num, gamma=2., alpha=.25, e=0.1):
         # reference : https://spaces.ac.cn/archives/4493
         nb_classes = len(classes_num)
         fianal_loss = (1-e) * balanced_fl + e * K.categorical_crossentropy(K.ones_like(prediction_tensor) / nb_classes, prediction_tensor)
-
         return fianal_loss
     return focal_loss_fixed
 
@@ -237,11 +262,16 @@ def smooth_label(labels, cls_num, on_value=0.99, epsilon=1e-8):
     return np.where(onehot > 0, on_value, (1 - on_value) / (cls_num - 1 + epsilon))
 
 def config_cpu():
-    tf.get_logger().setLevel('ERROR')
-    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-    os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+    #os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+    #os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+    #tf.get_logger().setLevel('ERROR')
+    try:
+        tf.config.experimental.set_visible_devices([], 'GPU')
+    except Exception as ee:
+        print(ee)
 
 def config_gpu():
+    tf.get_logger().setLevel('ERROR')
     gpus = tf.config.experimental.list_physical_devices('GPU')
     if gpus:
        try:

preproccessing/dataset_proc.py
@@ -3,6 +3,7 @@ import sys
 
 import argparse
 import os
+sys.path.append("./preproccessing/")
 import re
 import time
 from datetime import datetime
@@ -15,7 +16,7 @@ import pandas as pd
 import tensorflow as tf
 import logging
 from detect.mx_mtcnn.mtcnn_detector import MtcnnDetector
-from .serilize import BaseProc
+from serilize import BaseProc
 from pose import get_rotation_angle
 
 '''
@@ -48,12 +49,12 @@ def gen_boundbox(box, landmark):
         [(nose_x - w//2, nose_y - w//2), (nose_x + w//2, nose_y + w//2)]  # inner box
     ])
 
-def gen_face(detector, image, image_path=""):
+def gen_face(detector, image, image_path="", only_one=True):
     ret = detector.detect_face(image) 
     if not ret:
         raise Exception("cant detect facei: %s"%image_path)
     bounds, lmarks = ret
-    if len(bounds) > 1:
+    if only_one and len(bounds) > 1:
         print("!!!!!,", bounds, lmarks)
         raise Exception("more than one face %s"%image_path)
     return ret
@@ -62,8 +63,9 @@ MTCNN_DETECT = MtcnnDetector(model_folder=None, ctx=mx.cpu(0), num_worker=1, min
 
 class ProfileProc(BaseProc):
 
-    def __init__(self, name, data_dir, output_dir, overwrite=False, tf_dir="../data/", sample_rate=0.25):
+    def __init__(self, name, data_dir, output_dir, overwrite=False, tf_dir="../data/", sample_rate=0.25, extra_padding=0):
         BaseProc.__init__(self, name, data_dir, output_dir, COLUMS, overwrite, tf_dir, sample_rate)
+        self.extra_padding = extra_padding
 
     def _trans2tf_record(self, dataframe, trunck_num, sub_dir="train"):
         logging.info("not implemented %s"%sub_dir)
@@ -112,6 +114,9 @@ class WikiProc(ProfileProc):
         try:
             print(image_path)
             image = cv2.imread(image_path, cv2.IMREAD_COLOR)
+            if self.extra_padding:
+                extra_padding = self.extra_padding
+                image = cv2.copyMakeBorder(image, extra_padding, extra_padding, extra_padding, extra_padding, cv2.BORDER_CONSTANT)
             if not np.isnan(series.second_face_score):
                 raise Exception("secend face is not None~---%s~-%s- %s"%(series.name, series.age, image_path))
             bounds, lmarks = gen_face(detector, image, image_path)
@@ -183,7 +188,7 @@ class AsiaProc(WikiProc):
                 if not match:
                     continue
                 identity_id, age = match.groups() 
-                gender = 0 if int(identity_id) <= 7380 else 1  # 0: female 1: male
+                gender = FEMALE if int(identity_id) <= 7380 else MALE  # 0: female 1: male
                 paths.append(path)
                 genders.append(gender)
                 ages.append(int(age))
@@ -222,7 +227,7 @@ class UTKProc(AsiaProc):
                 if not match:
                     continue
                 age, gender, race = match.groups() 
-                gender = MALE if gender == "0" else FEMALE  # 0: female 1: male to 0:male
+                gender = MALE if gender == "0" else FEMALE
                 age = int(age)
                 paths.append(path)
                 genders.append(gender)
@@ -308,12 +313,12 @@ if __name__ == "__main__":
     if params.source == "wiki":
         WikiProc(params.input_path, params.dest, overwrite=True).process(nums=-1)
     elif params.source == "imdb":
-        ImdbProc(params.input_path, params.dest,  overwrite=True).process(nums=-1)
+        ImdbProc(params.input_path, params.dest, overwrite=True).process(nums=-1)
     elif params.source == "asia":
-        AsiaProc(params.input_path, params.dest,  overwrite=True).process(nums=-1)
+        AsiaProc(params.input_path, params.dest, overwrite=True).process(nums=-1)
     elif params.source == "utk":
-        UTKProc(params.input_path, params.dest,  overwrite=True).process(nums=-1)
+        UTKProc(params.input_path, params.dest, overwrite=True).process(nums=-1)
     elif params.source == "afad":
-        AFADProc(params.input_path, params.dest,  overwrite=True).process(nums=-1)
+        AFADProc(params.input_path, params.dest, extra_padding=100, overwrite=True).process(nums=-1)
     else:
         raise Exception("fatal source")